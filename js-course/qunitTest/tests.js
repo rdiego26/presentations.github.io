ffb6537e6bb2d95c30745bc33574a7dad0a18f95
rdiego26 <rdiego26@gmail.com>
2017-06-28 15:22:21 -0300

    Add JS course

---
+++ js-course/qunitTest/tests.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ramos on 11/03/15.
+ */
+QUnit.test( "1 == 1", function( assert ) {
+    assert.ok( 1 == "1", "Passed!" );
+});
+
+QUnit.test( "1 == 2", function( assert ) {
+   assert.ok( 1 == 2, 'Passed!' ) ;
+});
